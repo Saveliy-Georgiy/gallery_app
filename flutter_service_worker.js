408081809cc37b7f8e9ca1db5b518006af90d410
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2024-10-02 17:00:46 +0300

    Deploying web version to GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "5f909e3497d6cb935e7455097fd4eb9d",
+const RESOURCES = {"flutter_bootstrap.js": "865acfd702dec370c3dffb227fb194f5",
 "version.json": "3e2c1fe9914baa33f8d1087bf77c55ff",
 "index.html": "2d87e8349a117b8d5a22701dc11ab606",
 "/": "2d87e8349a117b8d5a22701dc11ab606",
-"main.dart.js": "67ce55bb2aec5b61983c715193b2d187",
+"main.dart.js": "889067834f400507d505fa5dc1eb90ea",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
