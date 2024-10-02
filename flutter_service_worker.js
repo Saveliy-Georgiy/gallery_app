8905ae8d10313c638e098112c5bb5a8a2abfd7fd
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2024-10-02 11:46:11 +0300

    Update GitHub Pages with latest changes from main

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "832f2c266598dda399034c5bda425370",
+const RESOURCES = {"flutter_bootstrap.js": "35497731793d9b770e9cf184bb0d2b98",
 "version.json": "3e2c1fe9914baa33f8d1087bf77c55ff",
 "index.html": "2d87e8349a117b8d5a22701dc11ab606",
 "/": "2d87e8349a117b8d5a22701dc11ab606",
