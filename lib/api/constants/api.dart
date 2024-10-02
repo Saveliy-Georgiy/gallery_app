7b5996d7e17ad7e13ac872174e3de1b1366dfcbe
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2024-10-02 13:44:19 +0300

    Change proxy for gh pages

--- lib/api/constants/api.dart
+++ lib/api/constants/api.dart
@@ -6,5 +6,5 @@ class Api {
   static const String pixabayApiKey = '46104914-9bb62297136b4f0c7cfa83b55';
   static const String proxyUrl = env == 'dev'
       ? 'https://thingproxy.freeboard.io/fetch'
-      : 'https://cors.sh';
+      : 'https://crossorigin.me';
 }
